15409c281987447dfe19be4cd7a6828d59864075
Felix <felix.husse@gmail.com>
2022-05-15 10:45:41 +0200

    initial commit

---
+++ fatalgram/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fatalgram.users.models import User
+from fatalgram.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
